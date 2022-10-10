4d1ca7c7c975cc58d389d837f831abdc61721d05
DEEZUZZA-OFFLINE <112531339+DEEZUZZA-OFFLINE@users.noreply.github.com>
2022-10-10 12:43:36 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -489,7 +489,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.message.edit_text(
             text=script.SOURCE_TXT,
             reply_markup=reply_markup,
-            parse_mode='html
+            parse_mode='html'
         )
     elif query.data == "manuelfilter":
         buttons = [[
